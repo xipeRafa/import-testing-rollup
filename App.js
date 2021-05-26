33a5b5e5aee9d18d4a271bcd7d48d6cf9eb3c1c8
rafa <rafaelsistem87@gmail.com>
2021-05-26 15:21:53 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+ import { Button } from 'react-rollup-ui/lib/index.es';
+
+const App = () => {
+      return (
+            <div>
+              <p>hello i am in NPM </p>
+              <Button text='Button'/>
+              <p>install me with: </p>
+              <p>yarn add react-rollup-ui</p>
+               
+            </div>
+        
+      )
+}
+
+export default App
